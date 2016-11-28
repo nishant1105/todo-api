f9432599261c325e918a8c186c95866a746a274c
Nishant Shrivastava <nishant.nish05@gmail.com>
2016-11-27 20:36:45 -0500

    refactor with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -28,23 +29,25 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-	todos.forEach(function (todo) {
-		if (todoID === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoID});
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-	//res.send('Asking for todo with id of ' + req.params.id);
+	
 });
 
 var todoNextId = 1;
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || 
+		!_.isString(body.description) || 
+		body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
 	console.log('description: ' + body.description);
