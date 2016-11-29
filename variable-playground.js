54ee8cb04708b8d2ac28a8de966395ab3c7d4240
Nishant Shrivastava <nishant.nish05@gmail.com>
2016-11-28 21:24:16 -0500

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+/*var person = {
+	name: 'Nishant',
+	age: 34
+};
+
+function updatePerson (obj) {
+	/*obj = {
+		name: 'Nishant',
+		age: 29
+	};
+	obj.age = 29;
+}
+
+updatePerson(person);
+console.log(person);*/
+
+// Array Example
+
+var grades = [15, 88];
+
+function addGrade(grades) {
+	//grades.push(55);
+	debugger;
+	grades = [12, 33, 99];
+}
+
+addGrade(grades);
+console.log(grades);
