4f951f0bdc58496888295f4d37b9d31e6ec0d67d
Nishant Shrivastava <nishant.nish05@gmail.com>
2016-12-03 18:55:37 -0500

    added user association

--- Playground area/basic-sqlite-database.js
+++ Playground area/basic-sqlite-database.js
@@ -22,13 +22,47 @@ var TODO = sequelize.define('todo', {
 		defaultValue: false
 	}
 });
-sequelize.sync({/*force: true*/}).then(function () {
+
+var user = sequelize.define('user', {
+	email: {
+		type: Sequelize.STRING
+	}
+});
+
+TODO.belongsTo(user);
+user.hasMany(TODO);
+
+sequelize.sync({
+//force: true
+}).then(function () {
 	console.log('Everything is synced.');
-	TODO.findById(1).then(function (todo) {
+	user.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
+	});
+	/*user.create({
+		email: 'nishant@examlpe.com'
+	}).then(function () {
+		return TODO.create({
+			description: 'Clean Yard'
+		});
+	}).then(function (todo) {
+		user.findById(1).then(function (user) {
+			user.addTodo(todo);
+		});
+	});
+	/*TODO.findById(1).then(function (todo) {
 		if(todo) {
 			console.log(todo.toJSON());
 		} else {
 			console.log('Todo not found');
 		}
-	});
+	});*/
 });
